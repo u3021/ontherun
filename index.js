96c5a09d3c56997d7e309dc4d1448d9ee31f6892
u3021 <sharman627@mysaddleback.org>
2025-10-14 10:44:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,1 @@
-if(mouseIsPressed?){
-console.log("67")};
-if(!mouseIsPressed?){
-console.clear()};
+import(file:///media/fuse/drivefs-b4f849054357c41b04c7d98a379bc156/root/KING%20SNEK(bossfight).html)
